4117d06fec6ee933164dec89eb7f772ccfe4f767
AlexandrRust <123_00@ukr.net>
2022-06-06 12:56:08 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,69 +1,40 @@
-import './css/styles.css';
-import { debounce } from 'throttle-debounce';
-import Notiflix from 'notiflix';
-import { fetchCountries } from './js/fetchCountries';
-
-const DEBOUNCE_DELAY = 300;
-const input = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list');
-const card = document.querySelector('.country-info');
+import getRefs from './js/functions/getRefs';
+import currentPage from './js/functions/currentPage';
+import { renderHomeHero, renderLibraryHero } from './js/components/hero';
 
 
+const refs = getRefs();
+const newCurrentPage = new currentPage();
 
-const debounceFunc = debounce(
-    DEBOUNCE_DELAY, 
-    (num) => {
-        const inputText = `${input.value}`;
-        const name = inputText.trim();
-        if (name.length === 0) {
-            countryList.innerHTML = "";
-            card.innerHTML = "";
-            return;
-        }
-        return fetchCountries(name);
-        
-    },
-    
-);
+///______ Первая загрузка страницы ______///
 
-input.addEventListener('input', debounceFunc);
+indexPageLoading();
 
+///________ Слушатели событий ________///
 
+refs.btnHome.addEventListener('click', indexPageLoading);
+refs.btnLibrary.addEventListener('click', LibraryPageLoading);
 
-export function makeCountryElement(countries) {
-      var countryElement = '';
-        
-        if (countries.length >= 10) {
-           Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-        } else if (countries.length >= 2 & countries.length < 10) {
-            card.innerHTML = "";
-             countryElement = countries.map((country) => `
-       <li class ="country-element">
-            <div class="country-img">
-                <img class="country-flag" src="${country.flags.svg}" alt="${country.name}" width="50" height="30">
-            </div>
-             <p class="country-name">${country.name}</p>
-        </li>
-    `).join("");
-        countryList.insertAdjacentHTML('afterbegin', countryElement);
-        } else {
-            countryList.innerHTML = "";
-            countryElement = countries.map((country) => 
-            `
-       <h3 class="country-title">
-        <img src="${country.flags.svg}" alt="${country.name}" width="50" height="30">
-            <span class="title-text">${country.name}</span>
-        </h3>
-        <p class="country-description">capital: <span class="country-description-text">${country.capital}</span></p>
-        <p class="country-description">population: <span class="country-description-text">${country.population}</span></p>
-        <p class="country-description">languages: <span class="country-description-text">${country.languages.map(language => language.name)}</span></p>
-    ` ).join("");
-            card.insertAdjacentHTML('afterbegin', countryElement);
-        }
-    
+///________ Загрузка index.html по нажатию на Home ______///
+
+function indexPageLoading() {
+    newCurrentPage.currentHome();
+    renderHomeHero()
+    //// функция которая рендерит main секцию для основной страницы//////
+    //// функция которая открывает модалку по нажатию на карточку в Home ////
 }
 
-export function fetchError(error) {
-    Notiflix.Notify.failure("Oops, there is no country with that name");
+///________ Загрузка страницы по нажатию на Library ______///
+
+function LibraryPageLoading() {
+    newCurrentPage.currentLibrary();
+    renderLibraryHero()
+    //// функция которая рендерит main секцию  для Library по нажатию на кнопки Watched и queue//////
+    //// функция которая открывает модалку по нажатию на карточку в Library ////
 }
 
+////______ Открытие модалки в Footer ______////
+
+function modalFooter() {
+    //// функции для открытия модалки Footer /////
+}
---
+++ src/js/functions/ApiService.js
@@ -0,0 +1,77 @@
+
+//метод filmApiService.fetchCards приймає два рядки: тип запиту (popular, searchQuerry, filmInfo) і пропси(відповідно day або week, рядок із запитом, id фільму)
+
+//На виході отримуємо Проміс, який треба перетворювати в json, обробити then і catch
+
+//При кожному запиті номер сторінки буде зростати на 1, тому не забуваємо метод resetPage() при "нових запитах"
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const KEY = '8f517ea939a3e5bbc59bc55f9e33cbf1';
+
+
+export default class FilmApiService{
+    constructor(){
+        this.page = 1;
+    }
+
+    async fetchCards(fetchType, props) {
+        let url = '';
+
+        if (fetchType === 'searchQuerry') {
+            console.log('fetchType: ', 'searchQuerry');
+               url = `${BASE_URL}/search/movie?api_key=${KEY}&language=en-US&query=${props}&page=${this.page}`;
+        }
+
+        if (fetchType === 'popularFilm') {
+            console.log('fetchType: ', 'popularFilm');
+               url = `${BASE_URL}/trending/movie/${props}?api_key=${KEY}&page=${this.page}`;
+        }
+ 
+        if (fetchType === 'filmInfo') {
+            console.log('fetchType: ', 'filmInfo');
+               url = `${BASE_URL}/movie/${props}?api_key=${KEY}&language=en-US`;
+        }
+
+        // console.log('url:', url);
+
+        return await fetch(url)
+            .then(response => response.json()).then((data) => {
+            
+                // console.log(data);
+  
+                this.incrementPage();
+                return data;            ;
+        });
+
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    setPage(newPage) {
+      this.page = newPage;  
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+//Axios чомусь видає помилку
+
+//Приклади:
+// filmApiService.fetchCards('searchQuerry', 'i')
+// 'popularFilm', 'day'
+// 'searchQuerry', 'fast'
+// 'filmInfo', '738229'
